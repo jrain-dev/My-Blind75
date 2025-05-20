4a04c0f90c02f902af07d83dd041ef6c3f869e3c
Jordan Rainford <jrain.dev@outlook.com>
2025-05-20 17:18:42 -0400

    Update LinkedList Problems.py
    The Edge cases got me on this problem. I forgot to add the case for if l1 and l2 had the same value, causing the code to run infinitely in that case, and then i neglected to use a dummy node to prevent the return value from being None. I should've been more detailed with my outline of potential cases to solve for.

--- LinkedList Problems.py
+++ LinkedList Problems.py
@@ -97,3 +97,36 @@ class Solution:
             current = current.next
 
         return head
+        
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        newList = []
+        l1 = list1
+        l2 = list2
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                newList.append(l1.val)
+                l1 = l1.next
+            elif l1.val > l2.val or l1.val == l2.val:
+                newList.append(l2.val)
+                l2 = l2.next
+
+        if l1 is None:
+            while l2:
+                newList.append(l2.val)
+                l2 = l2.next
+            
+        elif l2 is None:
+            while l1:
+                newList.append(l1.val)
+                l1 = l1.next
+        
+        dummy = ListNode(-1)
+        current = dummy
+
+        for n in newList:
+            current.next = ListNode(n)
+            current = current.next
+
+        return dummy.next
